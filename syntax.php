2a3e11e45112066268e76e3b5706f25f8bb3e6c4
Ikuo Obataya <i.obataya@gmail.com>
2018-06-19 22:08:28 +0900

    php7 support

--- syntax.php
+++ syntax.php
@@ -21,14 +21,14 @@ class syntax_plugin_pubmed extends DokuWiki_Syntax_Plugin {
  /**
   * Handle the match
   */
-  function handle($match, $state, $pos, &$handler){
+  function handle($match, $state, $pos, Doku_Handler $handler){
     $match = substr($match,9,-2);
     return array($state,explode(':',$match));
   }
  /**
   * Create output
   */
-  function render($mode, &$renderer, $data) {
+  function render($mode, Doku_Renderer $renderer, $data) {
     if ($mode!='xhtml')
       return false;
     list($state, $query) = $data;
